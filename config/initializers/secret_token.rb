3408cbf916dd39a42aef02225e836b73ebe89766
Michael Denomy <mdenomy@gmail.com>
2012-06-02 11:42:27 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WineNotes::Application.config.secret_token = '23ab4611f3f9cb59f4b9629005626a6c979a47df14f4024d90faf0b2a162458cc9c161a629e0dfd5af292a893c88462a82aef176503013f3de1825d2a9ab90c6'

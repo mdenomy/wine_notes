0984eeabbfe7566ad47c66b1dfab73e00ae2abe2
Michael Denomy <mdenomy@gmail.com>
2012-06-26 12:59:25 -0400

    Fixed failing tests from change to named routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,38 +3,36 @@ require 'spec_helper'
 describe "StaticPages" do
   let(:base_title) {"WineNotes"}
 
+  before { visit root_path }
   describe "home" do
     it "should have the content WineNotes" do
-      visit '/static_pages/home'
       page.should have_content 'WineNotes'
     end
 
     it "should have a title of Home" do
-      visit '/static_pages/home'
       page.should have_selector('title', :text => "#{base_title}" )
     end
   end
 
   describe "help" do
+    before { visit help_path }
     it "should have the content Help" do
-      visit '/static_pages/help'
       page.should have_content 'Help'
     end
 
     it "should have a title of Help" do
-      visit '/static_pages/help'
       page.should have_selector('title', :text => "#{base_title} | Help" )
     end
   end
 
   describe "about" do
+    before { visit about_path }
+
     it "should have the content About" do
-      visit '/static_pages/about'
       page.should have_content 'About'
     end
 
     it "should have a title of About" do
-      visit '/static_pages/about'
       page.should have_selector('title', :text => "#{base_title} | About" )
     end
   end

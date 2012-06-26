6eadb170c0fdf471ae9e9627a3bb24dd91f64400
Michael Denomy <mdenomy@gmail.com>
2012-06-26 18:25:19 -0400

    Implemented sign out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,13 +1,18 @@
 module SessionsHelper
 
+  def sign_out
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
+
   def sign_in(user)
      cookies.permanent[:remember_token] = user.remember_token
      self.current_user = user
-   end
+  end
 
-   def signed_in?
-     !current_user.nil?
-   end
+  def signed_in?
+    !current_user.nil?
+  end
 
   def current_user=(user)
     @current_user = user

5a6c7cc4ad68f9e5038c794385d6263a178ce581
Michael Denomy <mdenomy@gmail.com>
2012-07-07 13:51:31 -0400

    Removed debugger lines

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,10 @@
 class ReviewsController < ApplicationController
   def new
-    debugger
     @review = Review.new
     @wine = Wine.find_by_id(params[:wine_id])
   end
 
   def create
-    debugger
     @wine = Wine.find_by_id(params[:wine_id])
     @review = @wine.reviews.build(params[:review])
     @review.save
